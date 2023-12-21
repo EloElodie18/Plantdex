503d61700b12dd7a807a6061f82eb425601f26be
EloElodie18 <elodierichard@yahoo.fr>
2023-12-21 15:44:14 +0100

    4 - affichage plant + service

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -11,14 +11,22 @@ export class PageHomeComponent implements OnInit {
   //lui demander de ns injecter une instance du service plante
   //implementer l'interface Oninit qui est une methode intégrée
 
-  plantsToDisplay: Plant[] = []; //propriété que je nomme qui declare mon tableau
+  //jaime = true; fais partie de mon essai
+
+  plantsToDisplay: Plant[] = []; //propriété que je nomme qui declare mon tableau, créant la communication
 
   constructor(private plantsService: PlantsService) {}
 
-  ngOnInit(): void { //implementation qui permet un affichage des que le composant se lance
+  ngOnInit(): void {
+    //implementation qui permet un affichage des que le composant se lance
     this.plantsService.getPlants().subscribe((data) => {
       console.log(data);
       this.plantsToDisplay = [...data]; //(ecraser un tableau de la bonne maniere , vraie manière) ce qu'ils se passe: creer un tableau avec des données et on le colle à plantsToDisplay
-  });
-}
+    });
+  }
+
+  /*displayAfficher(){
+    this.jaime = !this.jaime;
+    }; methode de mon essai*/
+
 }
